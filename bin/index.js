7c8ef2de0ef46bf2ac7a3eb78a47b40b0d0bdca6
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-05 01:25:51 +0900

    [fix] fix forever entrypoint script

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-require('../lib/index.js');
+require("dotenv/config")
+const cron = require("node-cron")
+const {main} = require('../lib/index.js');
+
+const DEFAULT_SCHEDULE = '*/5 * * * *'
+const SCHEDULE = process.env.SCHEDULE ?? DEFAULT_SCHEDULE
+
+cron.schedule(SCHEDULE, main)
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import {push} from "./push";
-import "dotenv/config"
-import cron from "node-cron"
 
-const DEFAULT_SCHEDULE = '*/5 * * * *'
-const schedule = process.env.SCHEDULE ?? DEFAULT_SCHEDULE
-
-cron.schedule(schedule, main)
-
-function main() {
+export function main() {
     const {PUSH_API_URL, PUSH_API_KEY, DOMAIN_NAME} = process.env
 
     if (PUSH_API_URL == null || PUSH_API_KEY == null || DOMAIN_NAME == null) {
